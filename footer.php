e16bb88a670b05f9c1d5cbf5ddc5bcd6aa4e2dbe
Kareem Hunte <kareemhunte@yahoo.com>
2019-07-12 19:45:26 -0400

    Footer Updated
    -Address added to footer

--- footer.php
+++ footer.php
@@ -1,48 +1,50 @@
 	<footer>
 		<div class="outer-footer-container">
 			<div id="left-footer">
-				<div class="footer-container">
-					<h3>QUICK LINKS</h3>
+				<h3>Bleeding Hearts, Inc.</h3>
+				<div>P.O. Box 1246</div>
+				<div>Valley Steam, NY 11582</div>
+				<div>(347) 581-5404</div>
+			</div>
+			<div id="middle-footer">
+				<h3>QUICK LINKS</h3>
+				<ul>
+					<li>
+						<a href="#">DONATE</a>
+					</li>
+					<li>
+						<a href="<?php echo site_url('/events'); ?>">EVENTS</a>
+					</li>
+					<li>
+						<a href="<?php echo site_url('/about'); ?>">ABOUT</a>
+					</li>
+					<li>
+						<a href="<?php echo site_url('/contact'); ?>">CONTACT</a>
+					</li>
+				</ul>
+			</div>
+			<div id="right-footer">
+				<h3>FOLLOW US ON</h3>
+				<div id="social-media-links">
 					<ul>
-						<li>
-							<a href="#">DONATE</a>
+						<!--li>
+							<a href="#">
+								<i class="fab fa-facebook"></i>
+							</a>
 						</li>
 						<li>
-							<a href="<?php echo site_url('/events'); ?>">EVENTS</a>
-						</li>
+							<a href="#">
+								<i class="fab fa-youtube"></i>
+							</a>
+						</li-->
 						<li>
-							<a href="<?php echo site_url('/about'); ?>">ABOUT</a>
-						</li>
-						<li>
-							<a href="<?php echo site_url('/contact'); ?>">CONTACT</a>
+							<a href="https://www.instagram.com/bleedingheartsinc/" target="_blank">
+								<i class="fab fa-instagram"></i>
+							</a>
 						</li>
 					</ul>
 				</div>
-			</div>
-			<div id="right-footer">
-				<div class="footer-container">
-					<h3>FOLLOW US ON</h3>
-					<div id="social-media-links">
-						<ul>
-							<!--li>
-								<a href="#">
-									<i class="fab fa-facebook"></i>
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									<i class="fab fa-youtube"></i>
-								</a>
-							</li-->
-							<li>
-								<a href="https://www.instagram.com/bleedingheartsinc/" target="_blank">
-									<i class="fab fa-instagram"></i>
-								</a>
-							</li>
-						</ul>
-					</div>
-					<p>Website developed by <a href="https://github.com/Kreem97" target="_blank">Kareem Hunte</a></p>
-				</div>
+				<p>Website developed by <a href="https://github.com/Kreem97" target="_blank">Kareem Hunte</a></p>
 			</div>
 		</div>
 	</footer>
