4b35e650e5f37c53d8977148a810eeaaab4cb9b2
Kareem Hunte <kareemhunte@gmail.com>
2019-08-03 14:26:29 -0400

    Contact Page Started

--- page-contact.php
+++ page-contact.php
@@ -1,20 +1,43 @@
 <?php get_header(); ?>
 
 <main id="contact">
-CONNECT WITH US
-<!-- *Left side*
-Name *required*
-Email *required*
-Phone
-Message *required*
--->
-
-<!-- *Right side*
-Email
-Phone
-Address
--->
-
+	<div id="contact-head">CONNECT WITH US</div>
+	
+	<section>
+		<div>
+			<form>
+				<input type="text" class="form-name" placeholder="Name" required="required"/>
+				<input type="text" class="form-email" placeholder="Email" required="required"/>
+				<input type="text" class="form-phone" placeholder="Phone" required="required"/>
+				<input type="text" class="form-message" placeholder="Message" required="required"/>
+				<button type="submit">Send</button>
+			</form>
+		</div>
+		<div>
+			<label>Email<label/>
+			<label>bleedingheartsinc4k@gmail.com<label/>
+			<label>Phone<label/>
+			<label>(347) 581-5404<label/>
+			<label>Address<label/>
+			<label>
+				Bleeding Hearts, Inc.<br/>
+				P.O. Box 1246<br/>
+				Valley Steam, NY 11582
+			<label/>
+		</div>
+		<!-- *Left side*
+		Name *required*
+		Email *required*
+		Phone
+		Message *required*
+		-->
+		
+		<!-- *Right side*
+		Email
+		Phone
+		Address
+		-->
+	</section>
 </main>
 
 <?php get_footer(); ?>
